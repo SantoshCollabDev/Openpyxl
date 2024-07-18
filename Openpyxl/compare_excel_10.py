4c0132d8d0a0b61e6acf79ec2b7536f24ce7e600
SantoshKumarAndhugula <talk2_santosh@yahoo.com>
2024-07-18 12:01:54 +0530

    compare 2 excel files

---
+++ Openpyxl/compare_excel_10.py
@@ -0,0 +1,21 @@
+import openpyxl
+from openpyxl.styles import PatternFill
+
+file1 = openpyxl.load_workbook("Data_file1.xlsx")
+file2 = openpyxl.load_workbook("Data_file2.xlsx")
+
+file1_data = file1["master_data"]
+file2_data = file2["master_data"]
+
+fill_style = PatternFill(start_color="E60D0D", end_color="E60D0D", fill_type="solid")
+
+for row in file1_data.iter_rows():
+  for cell in row:
+    current_cell_value = cell.value
+    cell_location = cell.coordinate
+
+    if current_cell_value != file2_data[cell_location].value:
+      cell.fill = fill_style
+
+file1.save("compared_file.xlsx")
+
