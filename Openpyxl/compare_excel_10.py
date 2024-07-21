52d600f35a164addff5df7fefabc16c160de0717
SantoshKumarAndhugula <talk2_santosh@yahoo.com>
2024-07-21 10:43:21 +0530

    compare 2 excel files

--- Openpyxl/compare_excel_10.py
+++ Openpyxl/compare_excel_10.py
@@ -9,11 +9,13 @@ file2_data = file2["master_data"]
 
 fill_style = PatternFill(start_color="E60D0D", end_color="E60D0D", fill_type="solid")
 
+# cell.coordinate --- GET THE LOCATION OF CURRENT CELL
 for row in file1_data.iter_rows():
   for cell in row:
     current_cell_value = cell.value
     cell_location = cell.coordinate
 
+# cell.fill --- TO SET FILL STYLE
     if current_cell_value != file2_data[cell_location].value:
       cell.fill = fill_style
 
--- Openpyxl/vlookup_10.py
+++ Openpyxl/vlookup_10.py
@@ -6,6 +6,10 @@ db = openpyxl.load_workbook("daily_data.xlsx")
 master_sheet = mb['master_data']
 daily_sheet  = db['daily_data']
 
+# iter_rows() -->  TO ITERATE THROUGH THE ROWS OF AN EXCEL
+# .row  --> TO GET THE ROW NUMBER OF THE CURRENT ROW IN PROCESSING
+
+
 for i in daily_sheet.iter_rows():
   for j in master_sheet.iter_rows():
     if j[0].value == i[0].value:
